8db62da18f63d5493f05f6a52c125097869346e7
pooryairani <pooryaebooks@gmail.com>
2022-04-16 22:26:01 +0430

    first python project

---
+++ cylindrical.py
@@ -0,0 +1,9 @@
+radius = int(input('Please insert radius of Cylindrical :'))
+height = int(input('Please insert height of Cylindrical :'))
+vol = height * radius**2 * 3.14
+print('Cylindrical vol. is:',vol) #hajme ostovaneh
+area = 2 * 3.14 * radius * (height + 2) * (3.14 * radius**2)
+print('Cylindrical area is:',area) #masahate ostovaneh
+
+#developer ahmad taheri
+#ba tashakor
